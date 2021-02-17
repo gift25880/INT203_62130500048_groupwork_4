5e5aad76f8fcc083cd37886cab514adfacb70612
Pathinya <gift_01210@hotmail.com>
2021-02-17 12:00:20 +0700

    [UPDATE] search box and search filter

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ const app = {
                 { img: "./images/coit-tower.png", menuTitle: "Coit Tower", click: false },
                 { img: "./images/banana-split.png", menuTitle: "Banana Split", click: false }
             ],
+            searchClicked: false,
+            inputSearch: '',
+            notFound: false,
+            showImg: false
         }
     },
     methods: {
-        imgClicked(index) {
+        favClicked(index) {
             this.gallery[index].click = !this.gallery[index].click;
-        }
+        },
+        switchSearchClicked() {
+            this.searchClicked = !this.searchClicked;
+            if (this.searchClicked == false) {
+                this.inputSearch = '';
+            }
+        },
+        
     },
     computed: {
         photoAmount() {
@@ -19,6 +30,20 @@ const app = {
         },
         likeAmount() {
             return this.gallery.filter(n => n.click).length;
+        },
+        searchMenu() {
+            this.notFound = false;
+            if (this.inputSearch == '') {
+                return this.gallery;
+            } else {
+                let menu = this.gallery.filter(n => n.menuTitle.toLowerCase().includes(this.inputSearch.toLowerCase()));
+                if (menu == '') {
+                    this.notFound = true;
+                } else {
+                    return menu;
+                }
+            }
+            
         }
     }
 }
