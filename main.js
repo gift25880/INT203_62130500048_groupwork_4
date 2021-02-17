fa157dda395ed4dc2633695ffa9cae03ce79efb5
Pathinya <gift_01210@hotmail.com>
2021-02-17 18:19:44 +0700

    [UPDATE] fix image canvas bugs and add new menu items

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ const app = {
     data() {
         return {
             gallery: [
-                { img: "./images/strawberry-sundae.png", menuTitle: "Strawberry Banana & Cream", click: false, showImg: false },
-                { img: "./images/coit-tower.png", menuTitle: "Coit Tower", click: false, showImg: false },
-                { img: "./images/banana-split.png", menuTitle: "Banana Split", click: false, showImg: false }
+                { id: 0, img: "./images/strawberry-sundae.png", menuTitle: "Strawberry Banana & Cream", click: false },
+                { id: 1, img: "./images/coit-tower.png", menuTitle: "Coit Tower", click: false },
+                { id: 2, img: "./images/banana-split.png", menuTitle: "Banana Split", click: false },
+                { id: 3, img: "./images/chewy-chocky-fancy.png", menuTitle: "Chewy Chocky Fancy", click: false },
+                { id: 4, img: "./images/hot-fudge-bonanza-split.png", menuTitle: "Hot Fudge Bonanza Split", click: false },
+                { id: 5, img: "./images/nutty-crispy-tower.png", menuTitle: "Nutty Crispy Tower", click: false },
+                { id: 6, img: "./images/waffle-sundae.png", menuTitle: "Waffle Sundae", click: false },
+                { id: 7, img: "./images/chocolate-ring-a-ding.png", menuTitle: "Chocolate Ring-A-Ding", click: false },
+                { id: 8, img: "./images/earthquake.png", menuTitle: "The EarthQuake", click: false }
             ],
             searchClicked: false,
             inputSearch: '',
             notFound: false,
-            showImages: false
+            showImages: false,
+            currentIndex: 0
         }
     },
     methods: {
@@ -24,14 +31,10 @@ const app = {
         },
         close() {
             this.showImages = false;
-            for (let i = 0; i<this.gallery.length; i++) {
-                console.log(this.gallery[i].showImg);
-                this.gallery[i].showImg = false;
-            }
         },
         imgClicked(index) {
-            this.showImages = !this.showImages;
-            this.gallery[index].showImg = !this.gallery[index].showImg;
+            this.showImages = true;
+            this.currentIndex = index;
         }
 
     },
@@ -42,12 +45,15 @@ const app = {
         likeAmount() {
             return this.gallery.filter(n => n.click).length;
         },
+        searchAmount() {
+            return this.gallery.filter(n => n.menuTitle.toLowerCase().includes(this.inputSearch.toLowerCase())).length;
+        },
         searchMenu() {
             this.notFound = false;
             if (this.inputSearch == '') {
                 return this.gallery;
             } else {
-                let menu = this.gallery.filter(n => n.menuTitle.toLowerCase().includes(this.inputSearch.toLowerCase()));
+                menu = this.gallery.filter(n => n.menuTitle.toLowerCase().includes(this.inputSearch.toLowerCase()));
                 if (menu == '') {
                     this.notFound = true;
                 } else {
